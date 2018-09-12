f4cf6bae4ca7722e46ca1419f9aabd692cfbc5ad
Jean-Raphael Auclair-Soucy <Arganancer@gmail.com>
2018-09-12 12:48:12 -0400

    Removed workspace.xml

---
+++ TankGame/Assets/Scripts/Game/Pandemonium/Feature/FeatureEnemy.cs
@@ -0,0 +1,36 @@
+﻿using Game.Tank.Stimulus;
+using UnityEngine;
+
+namespace Game.Pandemonium.Feature
+{
+	public class FeatureEnemy : MonoBehaviour
+	{
+		private Transform rootTransform;
+		private DriverStimulus driverStimulus;
+		private CommanderStimulus commanderStimulus;
+		private TurretStimulus turretStimulus;
+
+		private void Awake()
+		{
+			InitializeComponent();
+		}
+
+		private void InitializeComponent()
+		{
+			rootTransform = transform.root;
+			driverStimulus = rootTransform.GetComponentInChildren<DriverStimulus>();
+			commanderStimulus = rootTransform.GetComponentInChildren<CommanderStimulus>();
+			turretStimulus = rootTransform.GetComponentInChildren<TurretStimulus>();
+		}
+
+		private void OnEnable()
+		{
+			//driverStimulus.OnStimulusActivated += OnEnemySeen;
+		}
+
+		private void OnEnemySeen(GameObject enemyGameObject)
+		{
+			
+		}
+	}
+}
