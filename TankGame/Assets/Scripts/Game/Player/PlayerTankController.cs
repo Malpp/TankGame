f606a66444615cf001459684175985457244f044
Malpp <cpt.ortan@gmail.com>
2018-09-16 17:38:49 -0400

    Fixed turret bug

--- TankGame/Assets/Scripts/Game/Player/PlayerTankController.cs
+++ TankGame/Assets/Scripts/Game/Player/PlayerTankController.cs
@@ -40,6 +40,12 @@ namespace Game.Player
 			
 			if(Input.GetKeyDown(KeyCode.Space))
 				turretController.Shoot();
+			
+			if(Input.GetKey(KeyCode.Q))
+				turretController.TurnLeft();
+			
+			if(Input.GetKey(KeyCode.E))
+				turretController.TurnRight();
 		}
 	}
 }
