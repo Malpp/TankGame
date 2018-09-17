123da94fb562ebcd3055b415d68e7bc87c69a41c
Malpp <cpt.ortan@gmail.com>
2018-09-16 21:35:23 -0400

    Added reload timer

--- TankGame/Assets/Scripts/Game/Tank/Ammo.cs
+++ TankGame/Assets/Scripts/Game/Tank/Ammo.cs
@@ -1,22 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Game.Tank
 {
 	public class Ammo : MonoBehaviour
 	{
-		[SerializeField]
-		private int ammo;
+		[SerializeField] private int ammo;
+		[SerializeField] private float reloadTime;
 
-		public bool CanShoot => ammo > 0;
+		private bool reloading = false;
+
+		public bool CanShoot => ammo > 0 && !reloading;
 
 		public void LoadAmmo()
 		{
 			ammo--;
+			StartCoroutine(ReloadAmmoCoroutine());
 		}
-		
+
 		public void Refill(int nbAmmo)
 		{
 			ammo += nbAmmo;
+		}
+
+		private IEnumerator ReloadAmmoCoroutine()
+		{
+			reloading = true;
+			yield return new WaitForSeconds(reloadTime);
+			reloading = false;
 		}
 	}
 }
