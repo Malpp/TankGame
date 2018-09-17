c12b7c598c51b71b2744cca3fb57105e8b9667f4
Malpp <cpt.ortan@gmail.com>
2018-09-16 22:51:35 -0400

    Added win text

--- TankGame/Assets/Scripts/Game/GameController.cs
+++ TankGame/Assets/Scripts/Game/GameController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
+using Game.Tank;
 using Game.Utils;
+using Game.Values;
 using UnityEngine;
 
 namespace Game
@@ -7,12 +9,28 @@ namespace Game
 	public class GameController : MonoBehaviour
 	{
 		private int score;
+		private GameObject player;
+		private GameObject enemy;
 
 		public event Delegates.EventHandlerT<int> OnScoreChanged;
+		public event Delegates.EventHandler OnPlayerWin;
+		public event Delegates.EventHandler OnAIWin;
 
 		private void OnEnable()
 		{
+			player = GameObject.FindWithTag(Tags.Player);
+			enemy = GameObject.FindWithTag(Tags.Enemy);
 			StartCoroutine(AddScoreCoroutine());
+		}
+
+		private void FixedUpdate()
+		{
+			if (player == null)
+				OnAIWin?.Invoke();
+
+			if (score < 60 && enemy != null) return;
+			OnPlayerWin?.Invoke();
+			enemy.GetComponentInChildren<Health>().Die();
 		}
 
 		private IEnumerator AddScoreCoroutine()
---
+++ TankGame/Assets/Scripts/Game/UI/WinnerText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Game.Values;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+	public class WinnerText : MonoBehaviour
+	{
+		private GameController gameController;
+		private Text text;
+
+		private void Awake()
+		{
+			gameController = GameObject.FindWithTag(Tags.GameController).GetComponent<GameController>();
+			text = GetComponent<Text>();
+		}
+
+		private void OnEnable()
+		{
+			gameController.OnAIWin += OnAiWin;
+			gameController.OnPlayerWin += OnPlayerWin;
+		}
+
+		private void OnPlayerWin()
+		{
+			SetVictoryText("Player");
+		}
+
+		private void OnAiWin()
+		{
+			SetVictoryText("AI");
+		}
+
+		private void SetVictoryText(String winner)
+		{
+			text.text = $"{winner} has won!";
+			text.enabled = true;
+		}
+	}
+}
